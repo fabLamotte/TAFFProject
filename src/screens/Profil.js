d6d22bebbaa6526764f9dfe851cc806fee81252f
fablamotte <devwebfabienlamottepro@outlook.fr>
2021-06-28 10:37:34 +0200

    Etablissement de l'architecture de base 28/06/2021 10:37

---
+++ src/screens/Profil.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Profil = () => {
+    return (
+        <View style={styles.container}>
+            <Text>profil</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
+
+export default Profil
