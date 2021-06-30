d6698b66f7f97dd9485cd1a339027197a88e57a7
Thalbeisen <thibault.halbeisendev@gmail.com>
2021-06-30 10:12:03 +0200

    Ajout de la map fonctionnele et repère user

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -2,9 +2,25 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import MapboxGL from '@react-native-mapbox-gl/maps'
 import Geolocation from '@react-native-community/geolocation'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+const AnnotationContent = ({
+   }) => {
+    return (
+        <View>
+            <Icon name="human" size={40} color="#C4585B" />
+        </View>
+    //   <View >
+    //     <View>
+    //       <Text></Text>
+    //     </View>
+    //     <Icon name="map-marker" color="#31C283" size={45} />
+    //   </View>
+    );
+   };
 
 MapboxGL.setAccessToken('sk.eyJ1IjoidGhpYmF1bHQtaGJzZGV2IiwiYSI6ImNrcWhybWd0bzBlbm8yb254ZmJycnViczEifQ.s-13ZSWVt4vrl00hZizGaw')
-
+MapboxGL.setTelemetryEnabled(false)
 const Map = () => {
 
     const [latData, setLatData] = useState('');
@@ -14,23 +30,30 @@ const Map = () => {
     console.log(latData)
     console.log(lonData)
 
+    
+
     return (
         <View style={styles.container}>
-            <MapboxGL.MapView
-                localizeLabels={true}
-                style={styles.map}
-            />
-            </View>
-    )}
+            <MapboxGL.MapView zoomLevel={14} localizeLabels={true} showUserLocation={true} style={styles.map} centerCoordinate={[lonData, latData]} >
+                <MapboxGL.Camera zoomLevel={16} animationMode={'flyTo'} animationDuration={0} centerCoordinate={[lonData, latData]} />
+                <MapboxGL.MarkerView coordinate={[lonData, latData]}>
+                    <View>
+                        <AnnotationContent />
+                    </View>
+                </MapboxGL.MarkerView>
+            </MapboxGL.MapView>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
 
-    container:{
-        flex:1,
-    },
-    map:{
+    container: {
         flex: 1,
-    }
+    },
+    map: {
+        flex: 1,
+    },
 })
 
 export default Map
