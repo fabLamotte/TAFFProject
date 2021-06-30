b09f762470b1be46c2381fcb59d4253e9dffbf82
Francois Demilly <francois.demilly@gmail.com>
2021-06-30 15:56:08 +0200

    added stack nav to profil

--- App.js
+++ App.js
@@ -2,15 +2,37 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 
 import Profil from './src/screens/Profil'
+import ParamsProfil from './src/screens/ParamsProfil'
 import Map from './src/screens/Map'
 import Covoiturage from './src/screens/Covoiturage'
 import BottomTab from './src/components/BottomTab'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator();
+
+
+export  const ProfilStackNav = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        options={{headerShown: false}}
+        name="Profil"
+        title="Profil"
+        component={Profil} />
+      <Stack.Screen
+        options={{headerTitle: "Paramètres de profil"}}
+        name="ParamsProfil"
+        title="ParamsProfil"
+        component={ParamsProfil} />
+
+    </Stack.Navigator>
+  )
+}
+
 
 const App = () => {
 
@@ -22,7 +44,7 @@ const App = () => {
           tabBar={(props, index) => <BottomTab key={index} {...props} />} 
         >
             <Tab.Screen name="Profile">
-              {() => <Profil />}
+              {() => <ProfilStackNav />}
             </Tab.Screen>
             <Tab.Screen name="Carte">
               {() => <Map />}
---
+++ src/screens/ParamsProfil.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {StyleSheet, View, Text } from 'react-native'
+
+const ParamsProfil = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text>Params</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+
+export default ParamsProfil
