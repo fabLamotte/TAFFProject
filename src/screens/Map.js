5597d9d75de66d5e6b5c4e1142e18c6272d74b0a
Thalbeisen <thibault.halbeisendev@gmail.com>
2021-07-01 09:12:06 +0200

    commit test

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -10,31 +10,43 @@ const AnnotationContent = ({
         <View>
             <Icon name="human" size={40} color="#C4585B" />
         </View>
-    //   <View >
-    //     <View>
-    //       <Text></Text>
-    //     </View>
-    //     <Icon name="map-marker" color="#31C283" size={45} />
-    //   </View>
     );
    };
 
 MapboxGL.setAccessToken('sk.eyJ1IjoidGhpYmF1bHQtaGJzZGV2IiwiYSI6ImNrcWhybWd0bzBlbm8yb254ZmJycnViczEifQ.s-13ZSWVt4vrl00hZizGaw')
 MapboxGL.setTelemetryEnabled(false)
+
+
+
 const Map = () => {
 
     const [latData, setLatData] = useState('');
     const [lonData, setLonData] = useState('');
+    const radiusData = useState('1500')
     Geolocation.getCurrentPosition(info => setLatData(info.coords.latitude));
     Geolocation.getCurrentPosition(info => setLonData(info.coords.longitude));
     console.log(latData)
     console.log(lonData)
 
+    const getHistoricMonumentBuildingsFromApi = async () => {
+        try {
+            let response = await fetch(
+                'https://heritage.toolforge.org/api/api.php?action=search&coord' + latData + ',' + lonData + '&radius=' + radiusData + '&stitem=total&format=json'
+            )
+            let jsonBuildings = await response.json();
+            console.log(jsonBuildings.monuments);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    
+    getHistoricMonumentBuildingsFromApi(latData, lonData);
+
     
 
     return (
         <View style={styles.container}>
-            <MapboxGL.MapView zoomLevel={14} localizeLabels={true} showUserLocation={true} style={styles.map} centerCoordinate={[lonData, latData]} >
+            <MapboxGL.MapView localizeLabels={true} style={styles.map}  >
                 <MapboxGL.Camera zoomLevel={16} animationMode={'flyTo'} animationDuration={0} centerCoordinate={[lonData, latData]} />
                 <MapboxGL.MarkerView coordinate={[lonData, latData]}>
                     <View>
